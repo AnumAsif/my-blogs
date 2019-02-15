65fd0186af8eba19e535f09e2a287786aee28d96
Anum Asif <anum@cockar.com>
2019-02-15 11:47:57 +0300

    Install and import flask-script package to run the app

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 from app import create_app, db
-
+from flask_script import Manager,Server
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server', Server)
 @manager.shell
 def make_shell_context():
     return dict(app=app, db=db)
